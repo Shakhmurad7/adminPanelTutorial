6dfdad113cc4a534e5f4c6af8cfab3a51c5a6c03
Shakhmurad <muradb356@gmail.com>
2023-12-27 22:35:53 +0400

    add cart in home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '../layout/Navbar'
 import '../index.css'
+import { FaAngleDown } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 function Home() {
   return (
     <>
@@ -8,7 +10,22 @@ function Home() {
     <Navbar/>
 
     <div className="Home-right">
-        <h1>companent</h1>
+      <div className="home-container-block-top">
+        <div className="home-block-top">
+            <div className="home-block-left">
+              <h3>USER</h3>
+              <h1>100</h1>
+              <p>See all users</p>
+            </div>
+            <div className="home-block-right">
+              <div className="home-block-icon">
+                <FaAngleDown />
+                <p>20 %</p>
+              </div>
+              <FaUser className='Fauser' />  
+            </div>
+        </div>
+      </div>
     </div>
     </div>
     </>
